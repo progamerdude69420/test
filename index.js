f6a87327b52b67fdb19704ff0ab01c0397681adb
yaigwa <106878630+progamerdude69420@users.noreply.github.com>
2024-03-14 21:11:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/chess/*", (req, res, next) => {
 app.get("/*", (req, res, next) => {
   //const pathSegments = req.path.split('/');
  // const rocketPath = pathSegments[2]
-  https.request(new URL("https://rocketbotroyale.winterpixel.io/" + req.path), (resp) => {
+  https.request(new URL("1v1.lol" + req.path), (resp) => {
     res.contentType(resp.headers["content-type"])
     resp.pipe(res)
   }).end()
